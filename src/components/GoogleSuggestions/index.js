ae99d110870154b5a244c82636a07bd01d5f0266
Chandrasekharrebba99 <chandui17cy028@gmail.com>
2022-12-31 10:43:00 +0000

    google search

---
+++ src/components/GoogleSuggestions/index.js
@@ -0,0 +1,75 @@
+import {Component} from 'react'
+import SuggestionItem from '../SuggestionItem'
+
+import './index.css'
+
+class GoogleSuggestions extends Component {
+  state = {
+    searchInput: '',
+    suggestionsList: this.props,
+  }
+
+  updateSearchInput = value => {
+    this.setState({
+      searchInput: value,
+    })
+  }
+
+  onChangeSearchInput = event => {
+    this.setState({
+      searchInput: event.target.value,
+    })
+  }
+
+  render() {
+    const {searchInput} = this.state
+    const {suggestionsList} = this.state
+
+    const searchResults = suggestionsList.suggestionsList.filter(
+      eachSuggestion =>
+        eachSuggestion.suggestion
+          .toLowerCase()
+          .includes(searchInput.toLowerCase()),
+    )
+    console.log(searchResults)
+
+    return (
+      <div className="app-container">
+        <div className="google-suggestions-container">
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/google-logo.png"
+            alt="google logo"
+            className="google-logo"
+          />
+          <div className="search-input-suggestions-container">
+            <div className="search-input-container">
+              <img
+                alt="search icon"
+                className="search-icon"
+                src="https://assets.ccbp.in/frontend/react-js/google-search-icon.png"
+              />
+              <input
+                type="search"
+                className="search-input"
+                placeholder="Search Google"
+                onChange={this.onChangeSearchInput}
+                value={searchInput}
+              />
+            </div>
+            <ul className="suggestions-list">
+              {searchResults.map(eachSuggestion => (
+                <SuggestionItem
+                  key={eachSuggestion.id}
+                  suggestionDetails={eachSuggestion}
+                  updateSearchInput={this.updateSearchInput}
+                />
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default GoogleSuggestions
